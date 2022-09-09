9f6c48a965f96b4ddae93fd6e893bc53e3d85083
Giovanni Gomes de Oliveira <giovannioliveira2002@gmail.com>
2022-09-09 11:57:51 -0300

    adjust-sectionOne

--- src/styles/theme/dark.ts
+++ src/styles/theme/dark.ts
@@ -32,6 +32,8 @@ export const darkTheme = {
     'bg-color': '#121214',
     'text-color': '#FFFFFF',
     'text-menu': '#FFFFFF',
+    'tertiary-500': '#0070f9',
+    'tertiary-700': '#0029f9',
   },
   desenvolvimento: '#FEE440',
   concluído: '#00875F',
